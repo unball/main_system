136b8593533fd76a3b8bc717b12e08a3d720899d
Igor Beduin <igor.rob.96@gmail.com>
2018-08-26 03:17:32 -0300

    Adding some docstrings so the code is correct with pep

--- world.py
+++ world.py
@@ -1,15 +1,23 @@
+"""World Module."""
+
+
 class World(object):
+    """Docstring for the class."""
+
     def __init__(self):
+        """Blank init."""
         pass
 
     def update(self):
+        """Docstring for the method."""
         self.past_info = self.instant_info
-        #self.instant_info = publisher
+        # self.instant_info = publisher
         pass
 
     def get_inst_info(self):
+        """Docstring for the method."""
         return self.instant_info
 
     def calc_velocities(self):
-        for obj in self.instant_info:
-            if self.instant_info[obj][pos] != None and self.past_info[obj][pos]
+        """Docstring for the method."""
+        pass
