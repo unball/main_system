fc3c67de3a4ea9d9be59f802a68022deb339e3a8
Igor Beduin <igor.rob.96@gmail.com>
2018-08-26 03:23:55 -0300

    Adding some general docstrings so the code agrees with pep257

--- ball.py
+++ ball.py
@@ -1,9 +1,16 @@
+"""Ball Module."""
+
+
 class Ball(object):
+    """Docstring for the class."""
+
     def __init__(self):
+        """Docstring for the method."""
         pass
 
     @property
     def inst_x(self):
+        """Docstring for the method."""
         return self.__inst_x
 
     @inst_x.setter
@@ -12,8 +19,9 @@ class Ball(object):
 
     @property
     def inst_y(self):
+        """Docstring for the method."""
         return self.__inst_y
 
-    @y.setter
+    @inst_y.setter
     def inst_y(self, y):
         self.__inst_y = y
