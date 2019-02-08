a4f491c7962e39739c5a6655b2f272bab27a022b
Igor Beduin <igor.rob.96@gmail.com>
2019-02-08 18:22:04 -0200

    Now it's necessary a TargetSelector to calculate targets from formation

---
+++ target_selector/target_selector.py
@@ -0,0 +1,10 @@
+from world import World
+
+class TargetSelector:
+    def __init__(self):
+        pass
+
+    def update(self, formation: list, world: World, centralZone = None):
+        self.formation = formation
+        self.world = world
+        self.centralZone = centralZone
