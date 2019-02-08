e977462254ac40a2883cb33be25d58a458d3d9b2
Igor Beduin <igor.rob.96@gmail.com>
2019-02-08 20:36:20 -0200

    According code to pep8 recommendations (ignoring some docstring errors)

--- target_selector/standard_selector.py
+++ target_selector/standard_selector.py
@@ -1,11 +1,12 @@
 from target_selector.target_selector import TargetSelector
 
+
 class StandardSelector(TargetSelector):
     def __init__(self):
         pass
 
-    def calcTargets(self, formation = None, world = None, centralZone = None):
-        if formation == None:
+    def calcTargets(self, formation=None, world=None, centralZone=None):
+        if formation is None:
             targets = list(player.target for player in self.formation)
             return targets
         elif isinstance(formation, list):
