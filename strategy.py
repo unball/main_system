e20d7addb532f2f6479cf3705b5609f0b74f95f6
makueba <makiander.abr@gmail.com>
2019-02-09 15:18:58 -0200

    added atackdecide object

--- strategy.py
+++ strategy.py
@@ -1,7 +1,7 @@
 """Strategy system module."""
 
 from firstleveldecider import FirstLvlDecider
-
+from secondleveldeciders.attack_decider import AttackDecider
 
 def error():
     """Print the standard error message for STRATEGY scope."""
@@ -16,16 +16,19 @@ class Strategy(object):
         self.coach = FirstLvlDecider()
         self.tactic = None
         self.formation = None
+        self.decider = AttackDecider()
 
     def plan(self, world):
         """Toplevel planner which contains all the deciders of the system."""
         self.world = world
         # TODO: VERIFICATION TEST FOR THE WORLD STATE
         self.tactic = self.coach.plan(self.world)
-        self.formation = self.tactic.find_formation(self.world)
-        self.targets = list(player.target for player in self.formation)
-        for player in self.formation:
-            print(player)
+       # self.formation = self.tactic.find_formation(self.world)
+        self.decider.setFormation(world, 5)
+
+        #self.targets = list(player.target for player in self.formation)
+        #for player in self.formation:
+         #   print(player)
 
     def get_targets(self):
         """Getter of each robot target planned."""
