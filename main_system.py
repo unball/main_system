1cbe8aa26deb05174fef509006c64bd902ecb858
Igor Beduin <igor.rob.96@gmail.com>
2019-02-10 18:50:11 -0200

    Not working. Callback not being called.

--- main_system.py
+++ main_system.py
@@ -1,24 +1,37 @@
+#!/usr/bin/env python
 """Main module system. World -> Strategy -> Control."""
 
-from world import World
-import world_standards
-from strategy import Strategy
-from controllers.controller import Controller
+import rospy
+
+from vision.msg import VisionMessage
+
+# import world_standards
+# from world import World
+# from strategy import Strategy
+# from controllers.controller import Controller
+
+
+def callback(data):
+    rospy.loginfo("UHUUUL")
+    print("blablablabla")
 
 
 def start_system():
     """Start the system in the 'main function'."""
-    world_state = World(world_standards.STANDARD3)
-    strategy_system = Strategy()
-    control_system = Controller()
-    # while True:
-    world_state.dummy_update()
-    strategy_system.plan(world_state)
-    targets = strategy_system.get_targets()
-    print(targets)
-    control_system.actuate(targets)
-    # Publish to communication node with ROS
+    # world_state = World(world_standards.STANDARD3)
+    # strategy_system = Strategy()
+    # control_system = Controller()
+
+    rospy.init_node('main_system')
+    rospy.Subscriber('/vision_output_topic', VisionMessage, callback)
+
+    # world_state.dummy_update()
+    # strategy_system.plan(world_state)
+    # targets = strategy_system.get_targets()
+    # control_system.actuate(targets)
+    rospy.spin()
 
 
 if __name__ == '__main__':
-        start_system()
+    print("---------- MAIN SYSTEM NODE STARTED ----------")
+    start_system()
