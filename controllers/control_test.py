e9d1ca09485d0da1a537213c637232a9dcf7929a
Igor Beduin <igor.rob.96@gmail.com>
2019-02-17 03:43:35 -0300

    Updating algorithm

--- controllers/control_test.py
+++ controllers/control_test.py
@@ -1,28 +1,36 @@
 import control
 import numpy as np
-
-x_r = None
-y_r = None
-th_r = None
-u1_r = None
-u2_r = None
-
-x = 0
-y = 0
-th = 0
-u1 = 0
-u2 = 0
+import matplotlib as plt
 
 
 def update_state():
     global dx
     global dy
     global dth
+    global du1
+    global du2
+    global state_vector
 
     dx = x - x_r
     dy = y - y_r
     dth = th - th_r
 
+    state_vector = [[dx],
+                    [dy],
+                    [dth]]
+
+
+x_r = 1
+y_r = 1
+th_r = np.pi
+u1_r = 1
+u2_r = 1
+
+x = 0
+y = 0
+th = 0
+u1 = 0
+u2 = 0
 
 A = [[0, 0, -1*np.sin(th_r)*u1_r],
      [0, 0, np.cos(th_r)*u1_r],
@@ -40,12 +48,19 @@ D = [[0, 0, 0],
      [0, 0, 0],
      [0, 0, 0]]
 
+
+global clsd_loop_poles
 clsd_loop_poles = [-5, -5, -25]
 
-K = control.place(A, B, clsd_loop_poles)
+for t in range(5):
+    update_state()
 
-state_vector = [[dx],
-                [dy],
-                [dth]]
+    A[0][2] = -1*np.sin(th_r)*u1_r
 
-saida = []
+    B[0][1] = np.cos(th_r)
+    B[1][1] = np.sin(th_r)
+
+    K = control.place(A, B, clsd_loop_poles)
+
+    du = -K * state_vector
+    output = C * state_vector
