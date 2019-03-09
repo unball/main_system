5f960b87899c477e7bca7126f0cc07417f27a8f3
Igor Beduin <igor.rob.96@gmail.com>
2019-03-09 05:45:48 -0300

    Defender with blocking ball elipse and striker with go to ball and shoot

--- strategy/players/defender.py
+++ strategy/players/defender.py
@@ -1,7 +1,9 @@
 """Defender module."""
 
+import numpy as np
+
 from strategy.players.player import Player
-from strategy.movements.mark import Mark
+# from strategy.movements.mark import Mark
 
 
 class Defender(Player):
@@ -11,7 +13,48 @@ class Defender(Player):
         """Responsible to instantiate the attributes of the parent class."""
         self.id = 1
 
-    def calc_target(self):
+    def calc_target(self, world):
         """Calculate it's own target based on world state."""
-        self.movement = Mark()
         print("Defender")
+        ########definir z = [a**2, b**2]###########
+        ballVel = np.array(world.ball.vel)
+        ballPos = np.array(world.ball.pos)
+
+
+        z = [.5**2, .3**2]
+        Goal = np.array([-.7, 0])
+        if (world.ball.vel[0]*world.fieldSide) > .15:
+            c =  [sum(z*(ballVel)**2), 2*np.dot(z*ballVel, ballPos-Goal), sum(z*(ballPos-Goal)**2) - np.prod(z)]
+            k = min(np.roots(c))
+            if k.imag == 0:
+                target = k*ballVel + ballPos
+                if(not self.ballInsideArea(ballPos)):
+                    target = list(target)
+                    target.append(np.arctan2(target[1]-world.robots[1].pos[1], target[0]-world.robots[1].pos[0]))
+                    return target
+                target[1] = target[1] + .1
+                target = list(target)
+                target.append(np.arctan2(target[1]-world.robots[1].pos[1], target[0]-world.robots[1].pos[0]))
+                return target        
+        #apenas fica entre a bola e o Goal
+        #if not ballInsideArea():
+        signal = 1
+        if ballPos[1]<0:
+            ballPos[1] = ballPos[1]* -1
+            signal = -1
+        if sum(z*(ballPos-Goal)) > 0:
+            target = list((np.sqrt(np.prod(z)/sum(z*(ballPos-Goal))) * (ballPos-Goal)) + Goal)
+            target[1] =  target[1]*signal
+            target.append(np.arctan2(target[1]-world.robots[1].pos[1], target[0]-world.robots[1].pos[0]))
+            return target
+
+        target = [(.15*world.fieldSide), (ballPos[1]+.1)]
+        target.append(np.arctan2(target[1]-world.robots[1].pos[1], target[0]-world.robots[1].pos[0]))
+        return target
+
+    def ballInsideArea(self, ballPos):
+        if abs(ballPos[0]) > .15:
+            return False
+        elif abs(ballPos[1]) > .35:
+            return False
+        return True
--- strategy/players/striker.py
+++ strategy/players/striker.py
@@ -1,8 +1,11 @@
 """Striker module."""
 
 from strategy.players.player import Player
-from strategy.movements.mark import Mark
+# from strategy.movements.mark import Mark
 
+
+#####################verify the robot number
+import numpy as np
 
 class Striker(Player):
     """Class docstring."""
@@ -11,11 +14,30 @@ class Striker(Player):
         """Responsible to instantiate the attributes of the parent class."""
         self.id = 2
 
-    def calc_target(self):
+    def calc_target(self, world):
         """Calculate it's own target based on world state."""
-        self.movement = Mark()
         print("Striker")
-        # Here, self._target is supposed to be instantiated with a Movement
+        ballPos = np.array(world.ball.pos)
+        robotPos = np.array(world.robots[2].pos)
+
+        if (robotPos[0]-ballPos[0])*world.fieldSide>0:
+            if abs(world.robots[2].th) != np.pi/2:
+                yPro = abs((-.75*world.fieldSide) - robotPos[0])*np.tan(world.robots[2].th) + robotPos[1]
+                if yPro > -.20 and yPro < .20:
+                    ballYPro = np.array([-.75*world.fieldSide, yPro]) - ballPos
+                    ballYProNorm = np.linalg.norm(ballYPro)
+                    robotYPro = np.array([-.75*world.fieldSide, yPro]) - robotPos
+                    robotYProNorm = np.linalg.norm(robotYPro)
+                    if robotYProNorm*ballYProNorm != 0:
+                        alpha = np.arccos(np.dot(robotYPro, ballYPro)/(robotYProNorm*ballYProNorm))
+                        if alpha < np.pi/12 and abs(robotPos[0] - ballPos[0]) < .2:
+                            target = [-7.5*world.fieldSide, 10*yPro]
+                            target.append(np.arctan2(target[1]-world.robots[2].pos[1], target[0]-world.robots[2].pos[0]))
+                            return target 
+
+        target = world.ball.pos
+        target.append(np.arctan2(target[1]-world.robots[2].pos[1], target[0]-world.robots[2].pos[0]))
+        return target
 
 
 if __name__ == "__main__":
