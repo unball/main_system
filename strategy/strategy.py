5ad13c212738c9e751cc4bed90f8a7a37f94fc3e
Igor Beduin <igor.rob.96@gmail.com>
2019-03-09 04:09:27 -0300

    Standard strategy with players

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -2,6 +2,9 @@
 
 from strategy.firstleveldecider import FirstLvlDecider
 from strategy.secondleveldeciders.attack_decider import AttackDecider
+from strategy.players.goalkeeper import Goalkeeper
+from strategy.players.striker import Striker
+from strategy.players.defender import Defender
 
 def error():
     """Print the standard error message for STRATEGY scope."""
@@ -15,19 +18,20 @@ class Strategy(object):
         """Init method."""
         self.firstLvlDec = FirstLvlDecider()
         self.tactic = None
-        self.formation = None
+        self.formation = [Goalkeeper(), Defender(), Striker()]
         self.decider = AttackDecider()
 
     def plan(self, world):
         """Toplevel planner which contains all the deciders of the system."""
         self.world = world
         # TODO: VERIFICATION TEST FOR THE WORLD STATE
-        self.tactic = self.firstLvlDec.plan(self.world)
-        
-        self.decider.setParams(world)
-        self.decider.setFormation(world)
-        self.targets = self.decider.updateTargets()
-        
+
+        # # Fuzzy
+        # self.tactic = self.firstLvlDec.plan(self.world)
+        # self.decider.setParams(world)
+        # self.decider.setFormation(world)
+        # self.targets = self.decider.updateTargets()
+        self.targets = list(player.calc_target(self.world) for player in self.formation)
 
     def get_targets(self):
         """Getter of each robot target planned."""
