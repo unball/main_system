5a439b7e1ea4fe979c2dbb05f537664682a79bd5
Igor Beduin <igor.rob.96@gmail.com>
2019-03-09 00:21:55 -0300

    Conversion Speeds to Motors working

--- controllers/utils.py
+++ controllers/utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+from communication.msg import comm_msg
+
+
 def convSpeeds2Motors(velocities):
+    norm_velocities = comm_msg()
     convertion = (512*19) / 100
     wheel_reduction = 3/1
     r = 0.03
@@ -11,8 +15,8 @@ def convSpeeds2Motors(velocities):
     max_motor_speed = (max_tics_per_s) / encoder_resolution
 
     for i in range(3):
-        lin_wR = (velocities.linear_vel[i] + (L/2)*velocities.linear_vel[i]) / r
-        lin_wL = (velocities.linear_vel[i] - (L/2)*velocities.linear_vel[i]) / r
+        lin_wR = (velocities.linear_vel[i] + (L/2)*velocities.angular_vel[i]) / r
+        lin_wL = (velocities.linear_vel[i] - (L/2)*velocities.angular_vel[i]) / r
 
         wR = lin_wR/(np.pi*2) # Rot/s
         wL = lin_wL/(np.pi*2) # Rot/s
@@ -20,13 +24,17 @@ def convSpeeds2Motors(velocities):
         wR = wR * wheel_reduction * convertion
         wL = wL * wheel_reduction * convertion
 
-        if np.fabs(wR) > max_motor_speed or np.fabs(wL) > max_motor_speed:
+        
+        if np.fabs(wR) > max_tics_per_s/100 or np.fabs(wL) > max_tics_per_s/100:
             if np.fabs(wR) >= np.fabs(wL):
-                wL = max_motor_speed * wL/np.fabs(wR)
-                wR = max_motor_speed * wR/np.fabs(wR)
+                wL = max_tics_per_s/100 * wL/np.fabs(wR)
+                wR = max_tics_per_s/100 * wR/np.fabs(wR)
             elif np.fabs(wL) >= np.fabs(wR):
-                wR = max_motor_speed * wR/np.fabs(wL)
-                wL = max_motor_speed * wL/np.fabs(wL)
+                wR = max_tics_per_s/100 * wR/np.fabs(wL)
+                wL = max_tics_per_s/100 * wL/np.fabs(wL)
 
+        norm_velocities.MotorA[i] = int(wL)
+        norm_velocities.MotorB[i] = int(wR)
 
-        return wR, wL
+        
+    return norm_velocities
