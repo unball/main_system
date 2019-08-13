af5ee44bbc380a440f932805ba0bf1e331f37f0e
Igor Beduin <igor.rob.96@gmail.com>
2019-08-13 01:50:03 -0300

    main_menu subclass that inherits from state

---
+++ main_menu.py
@@ -0,0 +1,12 @@
+from state import State
+from abc import ABC
+
+class MainMenu(State):
+    def __init__(self):
+        super().__init__()
+
+    def Update(self):
+        pass
+
+if __name__ == "__main__":
+    pass
