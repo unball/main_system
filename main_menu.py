5ee5947256238e554fa031dfd8137c59b8bce210
Igor Beduin <igor.rob.96@gmail.com>
2019-08-14 21:35:25 -0300

    Importing GameLoop to MainMenu

--- main_menu.py
+++ main_menu.py
@@ -1,11 +1,14 @@
 from state import State
 from abc import ABC
 
+from game_loop import GameLoop
+
 class MainMenu(State):
     def __init__(self):
         super().__init__()
 
     def update(self):
+        # Verificar Input Manager
         pass
 
     def next_state(self):
