03dbf44bc55f5bcb74bfc96c80054305bbef41e4
Igor Beduin <igor.rob.96@gmail.com>
2019-08-15 16:57:49 -0300

    Creating radio communiator class that inherits from communication class

---
+++ communication/radio_comm.py
@@ -0,0 +1,9 @@
+from communication.communication_system import ROSCommunicator
+
+class RadioCommunicator(ROSCommunicator):
+    def __init__(self):
+        super().__init__()
+
+    def send(self, msg):
+        pass
+    
