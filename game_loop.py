7cee2564f94880a9d4894fe5ed03fb01828475d0
Igor Beduin <igor.rob.96@gmail.com>
2019-08-14 21:35:34 -0300

    Creating GameLoop state

---
+++ game_loop.py
@@ -0,0 +1,20 @@
+from state import State
+from abc import ABC
+
+class GameLoop(State):
+    def __init__(self):
+        super().__init__()
+
+    def update(self):
+        # Vision System
+        # Measurement System
+        # Strategy System
+        # Control System
+        # Communication System
+        pass
+
+    def next_state(self):
+        pass
+
+if __name__ == "__main__":
+    pass
