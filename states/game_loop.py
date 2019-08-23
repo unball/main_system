b176b67e35c432a9c3d6cabd496ded363d333acd
Luiz <labm1997@gmail.com>
2019-08-23 12:32:28 -0300

    Os componentes do sistema agora são instanciados na gameThread e não no gameLoop, isso permite que a configuração possa ser feita em outro estado

--- states/game_loop.py
+++ states/game_loop.py
@@ -9,25 +9,21 @@ from vision.mainVision.mainVision import MainVision
 import time
 
 class GameLoop(State):
-    def __init__(self):
-        super().__init__()
-        self.__visionSystem = MainVision()
-        self.__strategySystem = Strategy()
-        self.__controlSystem = ssRegulator()
-        self.__radioComm = RadioCommunicator()
+    def __init__(self, thread):
+        super().__init__(thread)
 
     def update(self):
         # Vision System
         t0 = time.time()
-        self.__visionSystem.update()
-        self.__strategySystem.plan(static_classes.world)
-        targets, spin = self.__strategySystem.get_targets()
-        velocities = self.__controlSystem.actuate(targets, static_classes.world)
-        self.__radioComm.send(velocities)
+        self.thread.visionSystem.update()
+        self.thread.strategySystem.plan(static_classes.world)
+        targets, spin = self.thread.strategySystem.get_targets()
+        velocities = self.thread.controlSystem.actuate(targets, static_classes.world)
+        self.thread.radioComm.send(velocities)
         print(time.time()-t0)
 
-    def next_state(self):
-        pass
+#    def next_state(self):
+#        pass
 
 if __name__ == "__main__":
     pass
