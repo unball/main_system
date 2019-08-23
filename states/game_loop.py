f93a78feceef6a8b4470b93830f3254e5aae900a
Luiz <labm1997@gmail.com>
2019-08-22 21:45:35 -0300

    Visão adicionada ao gameloop

--- states/game_loop.py
+++ states/game_loop.py
@@ -5,20 +5,26 @@ from strategy.strategy import Strategy
 from controllers.ssRegulator import ssRegulator
 from communication_system.radio_comm import RadioCommunicator
 from statics import static_classes
+from vision.mainVision.mainVision import MainVision
+import time
 
 class GameLoop(State):
     def __init__(self):
         super().__init__()
+        self.__visionSystem = MainVision()
         self.__strategySystem = Strategy()
         self.__controlSystem = ssRegulator()
         self.__radioComm = RadioCommunicator()
 
     def update(self):
         # Vision System
+        t0 = time.time()
+        self.__visionSystem.update()
         self.__strategySystem.plan(static_classes.world)
         targets, spin = self.__strategySystem.get_targets()
         velocities = self.__controlSystem.actuate(targets, static_classes.world)
         self.__radioComm.send(velocities)
+        print(time.time()-t0)
 
     def next_state(self):
         pass
