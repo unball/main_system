7425b85963352ba036deeacf0138ea5a384ed13c
Luiz <labm1997@gmail.com>
2019-08-23 12:33:41 -0300

    mainMenu implementa o estado de configuração, no update a UI é atualizada com o frame de configuração mais recente

--- states/main_menu.py
+++ states/main_menu.py
@@ -1,18 +1,50 @@
 from states.state import State
 from abc import ABC
+from gi.repository import GLib
+import cv2
+import time
+import vision.cameras
+import gui.mainWindow
 
 from states.game_loop import GameLoop
 
 class MainMenu(State):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, thread):
+        super().__init__(thread)
+        
+    def update_stats(self, processing_time, loop_time):
+        gui.mainWindow.MainWindow().getObject("stats_label").set_text("Tempo de processamento: {:3.0f} ms\nTempo de loop: {:3.0f} ms".format(processing_time*1000, loop_time*1000))
 
     def update(self):
-        # Verificar Input Manager
-        pass
+        loop_time = time.time()
+        
+        frame = vision.cameras.uiCamerasList().getFrame()
+        if frame is None:
+            #GLib.idle_add(gui.mainWindow.MainWindow().ui_frame.clear_image)
+            #time.sleep(0.03)
+            return
+            
 
-    def next_state(self):
-        return GameLoop()
+        fr = gui.mainWindow.MainWindow().selectedFrameRenderer
+        if fr is None: return
+        
+        #frame = frame_.copy()
+        #frame_resized = cv2.resize(frame, (round(frame.shape[1]/frame.shape[0]*600),600))
+        processing_time = time.time()
+        frame_processed = fr.transformFrame(frame, frame)
+        processing_time = time.time()-processing_time
+        
+        height, width, depth = frame_processed.shape
+        GLib.idle_add(gui.mainWindow.MainWindow().ui_frame.do_update_frame, (frame_processed, width, height, depth))
+        
+        #time.sleep(0.03)
+            
+        loop_time = time.time()-loop_time
+        GLib.idle_add(self.update_stats, processing_time, loop_time)
+        
+
+#    def next_state(self):
+#        return GameLoop(self.thread)
 
 if __name__ == "__main__":
     pass
