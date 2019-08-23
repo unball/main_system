7c317e0dda28da8799178fed68ae606783828980
Luiz <labm1997@gmail.com>
2019-08-23 12:29:39 -0300

    Correção na identação

--- gui/signals.py
+++ gui/signals.py
@@ -5,36 +5,41 @@ import vision.cameras
 import interfaces.gamecommands
 
 class Signals:
-	def __init__(self):
-		self.pageSelected = 0
-		
-	def onDestroy(self, *args):
-		Gtk.main_quit()
-		
-	def list_cameras(self, widget_list):
-		vision.cameras.uiCamerasList().updateCameras(widget_list)
-		
-	def select_camera(self, widget, widget_selected):
-		vision.cameras.uiCamerasList().setCamera(widget_selected.index)
-		
-	def onConfigPageChange(self, page, widget, num):
-		self.pageSelected = num
-		gui.mainWindow.MainWindow().set_frame_renderer(num)
-	
-	def gameCommands_IniciodeJogo(self, widget):
-		interfaces.gamecommands.gameCommands().IniciodeJogo()
-	
-	def gameCommands_mudarTexto(self, widget):
-		interfaces.gamecommands.gameCommands().mudarTexto()
-	
-	def gameCommands_ally(self, widget):
-		interfaces.gamecommands.gameCommands().ally()
-	
-	def gameCommands_enemy(self, widget):
-		interfaces.gamecommands.gameCommands().enemy()
-	
-	def gameCommands_left(self, widget):
-		interfaces.gamecommands.gameCommands().left()
-	
-	def gameCommands_right(self, widget):
-		interfaces.gamecommands.gameCommands().right()
+    def __init__(self):
+        self.pageSelected = 0
+        
+    def onDestroy(self, *args):
+        Gtk.main_quit()
+        
+    def list_cameras(self, widget_list):
+        vision.cameras.uiCamerasList().updateCameras(widget_list)
+        
+    def select_camera(self, widget, widget_selected):
+        vision.cameras.uiCamerasList().setCamera(widget_selected.index)
+        
+    def onConfigPageChange(self, page, widget, num):
+        self.pageSelected = num
+        gui.mainWindow.MainWindow().set_frame_renderer(num)
+    
+    def mainPageChange(self, widget, page):
+        gameThread = gui.mainWindow.MainWindow().gameThread
+        if gameThread is not None:
+            gameThread.set_state(widget.get_stack().get_visible_child_name())
+    
+    def gameCommands_IniciodeJogo(self, widget):
+        interfaces.gamecommands.gameCommands().IniciodeJogo()
+    
+    def gameCommands_mudarTexto(self, widget):
+        interfaces.gamecommands.gameCommands().mudarTexto()
+    
+    def gameCommands_ally(self, widget):
+        interfaces.gamecommands.gameCommands().ally()
+    
+    def gameCommands_enemy(self, widget):
+        interfaces.gamecommands.gameCommands().enemy()
+    
+    def gameCommands_left(self, widget):
+        interfaces.gamecommands.gameCommands().left()
+    
+    def gameCommands_right(self, widget):
+        interfaces.gamecommands.gameCommands().right()
