153a1c880802756b95c3c2cec5a6a2ddd8ba43fb
Luiz <labm1997@gmail.com>
2019-09-25 12:05:46 -0300

    FrameRenderer da estratégia atualizado para corresponder ao do mundo

--- strategy/frameRenderer.py
+++ strategy/frameRenderer.py
@@ -7,39 +7,48 @@ from statics.world.game_elements.robot import Robot
 
 import cv2
 import numpy as np
+import math
 
 class parametrosEstrategia(gui.frameRenderer.frameRenderer):
 
     def __init__(self, vision):
         super().__init__(vision, "fr_strategy_notebook")
+        self.frameShape = (520,640)
+        
+    def draw_rectangle(self, frame, position, size, angle, color=(0,255,0)):
+        rect = (position, size, -angle*180/np.pi)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(frame,[box],0,color,2)
+        cv2.circle(frame, (int(position[0]+size[0]/2*math.cos(-angle)), int(position[1]+size[0]/2*math.sin(-angle))), 3, (255,255,255), -1)
     
     def transformFrame(self, frame, originalFrame):
-        height, width = (520,640)
+        height, width = self.frameShape
         frame = np.zeros((height,width,3), np.uint8)
         
-        delta_p,_ = meters2pixel((self.parent.decider.delta,0), (height,width))
-        delta_n,_ = meters2pixel((-self.parent.decider.delta,0), (height,width))
-        cv2.line(frame, (delta_p,0), (delta_p,height), (150,150,150), 1)
-        cv2.line(frame, (delta_n,0), (delta_n,height), (150,150,150), 1)
+        ellipseCenter = meters2pixel((0.75*world.fieldSide, 0), self.frameShape)
+        ellipseAxis = (int(0.4*width/1.6),int(0.2*height/1.3))
+        cv2.ellipse(frame, ellipseCenter, ellipseAxis, 0, 0, 360, (100,100,100), 1)
+        blkballline,_ = meters2pixel((0.55*world.fieldSide,0), (height,width))
+        cv2.line(frame, (blkballline,0), (blkballline,height), (100,100,100), 1)
         
         for robot in world.robots:
             if robot.pos[0] is not None and robot.pos[1] is not None:
                 position = meters2pixel(robot.pos, (height,width))
-                rect = (position,(25,25),robot.th)
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
-                cv2.drawContours(frame,[box],0,(0,255,0),2)
+                
+                self.draw_rectangle(frame, position, (25,25), robot.th, color=(0,255,0))
+                
                 if robot.entity is not None:
-                    cv2.putText(frame, str(robot.entity)[0], (int(position[0])-10, int(position[1])+10), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255))
+                    cv2.putText(frame, str(robot.entity)[0], (int(position[0])-7, int(position[1])+5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255))
                 
                 robot.discretize(0.01)
                 if len(robot.trajectory) > 0:
-                    #print(np.array([meters2pixel(x, (height,width)) for x in robot.trajectory[0]]))
                     cv2.polylines(frame,[np.array([meters2pixel(x, (height,width)) for x in robot.trajectory[0]])],False,(255,255,255),1)
         
         bola = world.ball
         ballpos = meters2pixel(bola.pos, (height,width))
         cv2.circle(frame, ballpos, 5, (255,0,0), -1)
+        
         
         return frame
 
